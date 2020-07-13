61f1d95c86a9a7944b6a41e6b7622108d567559f
Adrian <aagonzalez28@miners.utep.edu>
2020-07-13 10:36:13 -0600

    Initial commit

---
+++ app/src/main/java/edu/utep/cs5381/tappydefender/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.utep.cs5381.tappydefender;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
