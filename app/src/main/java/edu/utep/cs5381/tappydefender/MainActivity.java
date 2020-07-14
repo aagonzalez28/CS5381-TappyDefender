d0f0f8fde34fc3952f31f792b65b550071f579b4
Adrian <aagonzalez28@miners.utep.edu>
2020-07-14 07:17:47 -0600

    Chapter 2 Complete

--- app/src/main/java/edu/utep/cs5381/tappydefender/MainActivity.java
+++ app/src/main/java/edu/utep/cs5381/tappydefender/MainActivity.java
@@ -1,14 +1,37 @@
 package edu.utep.cs5381.tappydefender;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity
+        implements View.OnClickListener{
 
+    @Override
+    public void onClick(View view) {
+        // must be the Play button.
+        // Create a new Intent object
+        Intent i = new Intent(this, GameActivity.class);
+        // Start our GameActivity class via the Intent
+        startActivity(i);
+        // Now shut this activity down
+        finish();
+
+    }
+
+    // This is the entry point to our game
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Here we set our UI layout as the view
         setContentView(R.layout.activity_main);
+
+        // Get a reference to the button in our layout
+        final Button buttonPlay =
+                (Button)findViewById(R.id.buttonPlay);
+        // Listen for clicks
+        buttonPlay.setOnClickListener(this);
     }
 }
